cc5af7d78d56e0e31eaf88241a36387bf82b3059
tandpfun <28990589+tandpfun@users.noreply.github.com>
2022-12-11 15:54:55 -0800

    feat: transition mobile menu, formatting

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,7 +1,9 @@
 import Link from "next/link";
-import { FunctionComponent, useState} from "react";
+import { FunctionComponent, useState } from "react";
 
 import { Logo } from "./Logo";
+import { Close } from "./icons/Close";
+import { Menu } from "./icons/Menu";
 
 export const Navbar: FunctionComponent = () => {
 	const [open, setOpen] = useState(false);
@@ -34,36 +36,39 @@ export const Navbar: FunctionComponent = () => {
 				</div>
 			</div>
 
-			<div className={"flex flex-col justify-start items-end sm:hidden px-4 py-3 absolute right-0 h-screen "+ (open ? "bg-theme-shadow":"")}>
+			<div
+				className={
+					"flex flex-col justify-start items-end sm:hidden px-4 py-3 absolute right-0 h-screen transition-colors " +
+					(open ? "bg-theme-shadow" : "")
+				}>
 				<div className="right-0">
-					<button type="button" className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:opacity-50 " aria-controls="mobile-menu" aria-expanded="false" onClick={() => {setOpen(!open)}}>			
-						{open ? 
-						<svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-							<path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-						</svg>
-						: 
-						<svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true">
-							<path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-						</svg>
-						}
+					<button
+						type="button"
+						className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:opacity-50 "
+						aria-controls="mobile-menu"
+						aria-expanded="false"
+						onClick={() => {
+							setOpen(!open);
+						}}>
+						{open ? <Close className="block h-6 w-6" /> : <Menu className="block h-6 w-6" />}
 					</button>
 				</div>
-				{open && <div className="flex flex-col items-end">
-							<Link href="#about" className="hover:opacity-80 transition-opacity p-2">
-								About
-							</Link>
-							<Link href="#faq" className="hover:opacity-80 transition-opacity p-2">
-								FAQ
-							</Link>
-							<Link href="#contact" className="hover:opacity-80 transition-opacity p-2">
-								Contact
-							</Link>
-							<Link
-								className="bg-theme-orange px-6 py-3 text-black hover:opacity-80 transition-opacity rounded-sm p-3 my-2"
-								href="/apply">
-								Apply
-							</Link>
-				</div>}
+				<div className={"flex flex-col items-end transition-[width] overflow-hidden " + (open ? "w-36" : "w-0")}>
+					<Link href="#about" className="hover:opacity-80 transition-opacity p-2">
+						About
+					</Link>
+					<Link href="#faq" className="hover:opacity-80 transition-opacity p-2">
+						FAQ
+					</Link>
+					<Link href="#contact" className="hover:opacity-80 transition-opacity p-2">
+						Contact
+					</Link>
+					<Link
+						className="bg-theme-orange px-6 py-3 text-black hover:opacity-80 transition-opacity rounded-sm p-3 my-2"
+						href="/apply">
+						Apply
+					</Link>
+				</div>
 			</div>
 		</div>
 	);
